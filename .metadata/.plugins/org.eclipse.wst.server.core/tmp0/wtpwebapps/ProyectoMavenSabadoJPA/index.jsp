a703d1e212e0647fd71debe847831dbf1ba62ba9
elyost <sayitjust@gmail.com>
2024-06-22 09:25:36 -0500

    subiendo desde un repositorio local con git

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/ProyectoMavenSabadoJPA/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body bgcolor="#c5dec9">
+<h2>Proyecto Maven con JPA Web.</h2>
+</body>
+</html>
---
+++ ProyectoMavenSabadoJPA/src/main/java/Interfaces/ICliente.java
@@ -0,0 +1,13 @@
+package Interfaces;
+
+import java.util.List;
+
+import model.TblCliente;
+
+public interface ICliente {
+public void RegistrarCliente (TblCliente cliente);
+public void ActualizarCliente (TblCliente cliente);
+public void EliminarCliente (TblCliente cliente);
+public TblCliente BuscarCliente (TblCliente cliente);
+public List<TblCliente> ListadoCliente();
+}
